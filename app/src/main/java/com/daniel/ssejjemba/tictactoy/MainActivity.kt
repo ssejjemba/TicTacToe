b4b6ba2b221db14a9d6dcdad13f0ac8ee8d3e820
Daniel <ssejjembadan@gmail.com>
2019-03-09 18:37:08 +0300

    Added play game logic and select winner

--- app/src/main/java/com/daniel/ssejjemba/tictactoy/MainActivity.kt
+++ app/src/main/java/com/daniel/ssejjemba/tictactoy/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.daniel.ssejjemba.tictactoy
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,34 +18,111 @@ class MainActivity : AppCompatActivity() {
     protected fun buClick(view:View){
         val btnPressed = view as Button
 
+        var cellId:Int = 0
         when(btnPressed.id){
             R.id.bu1 -> {
-                Log.d("ButtonPress", "Button 1 pressed")
+                cellId = 1
+                Log.d("ButtonPress", "Button $cellId pressed")
             }
             R.id.bu2 -> {
-                Log.d("ButtonPress", "Button 2 pressed")
+                cellId = 2
+                Log.d("ButtonPress", "Button $cellId pressed")
             }
             R.id.bu3 -> {
-                Log.d("ButtonPress", "Button 3 pressed")
+                cellId = 3
+                Log.d("ButtonPress", "Button $cellId pressed")
             }
             R.id.bu4 -> {
-                Log.d("ButtonPress", "Button 4 pressed")
+                cellId = 4
+                Log.d("ButtonPress", "Button $cellId pressed")
             }
             R.id.bu5 -> {
-                Log.d("ButtonPress", "Button 5 pressed")
+                cellId = 5
+                Log.d("ButtonPress", "Button $cellId pressed")
             }
             R.id.bu6 -> {
-                Log.d("ButtonPress", "Button 6 pressed")
+                cellId = 6
+                Log.d("ButtonPress", "Button $cellId pressed")
             }
             R.id.bu7 -> {
-                Log.d("ButtonPress", "Button 7 pressed")
+                cellId = 7
+                Log.d("ButtonPress", "Button $cellId pressed")
             }
             R.id.bu8 -> {
-                Log.d("ButtonPress", "Button 8 pressed")
+                cellId = 8
+                Log.d("ButtonPress", "Button $cellId pressed")
             }
             R.id.bu9 -> {
-                Log.d("ButtonPress", "Button 9 pressed")
+                cellId = 9
+                Log.d("ButtonPress", "Button $cellId pressed")
             }
+
+        }
+
+        playGame(cellId, btnPressed)
+    }
+
+    var player1 = ArrayList<Int>()
+    var player2 = ArrayList<Int>()
+    var activePlayer = 1
+
+    private fun playGame(callId:Int, btnPressed:Button){
+        if(activePlayer == 1){
+            btnPressed.text = "X"
+            btnPressed.setBackgroundColor(Color.GREEN)
+            player1.add(callId)
+            activePlayer = 2
+        }else{
+            btnPressed.text = "O"
+            btnPressed.setBackgroundColor(Color.BLUE)
+            player2.add(callId)
+            activePlayer = 1
+        }
+
+        btnPressed.isEnabled = false
+        checkWinner()
+
+
+    }
+
+    private fun checkWinner() {
+        var winner = -1
+
+        //row 1
+        if(player1.contains(1)&&player1.contains(2)&&player1.contains(3)) winner = 1
+        if(player2.contains(1)&&player2.contains(2)&&player2.contains(3)) winner = 2
+
+        //row 2
+        if(player1.contains(4)&&player1.contains(5)&&player1.contains(6)) winner = 1
+        if(player2.contains(4)&&player2.contains(5)&&player2.contains(6)) winner = 2
+
+        //row 3
+        if(player1.contains(7)&&player1.contains(8)&&player1.contains(9)) winner = 1
+        if(player2.contains(7)&&player2.contains(8)&&player2.contains(9)) winner = 2
+
+
+        //col 1
+        if(player1.contains(1)&&player1.contains(4)&&player1.contains(7)) winner = 1
+        if(player2.contains(1)&&player2.contains(4)&&player2.contains(7)) winner = 2
+
+        //col 2
+        if(player1.contains(2)&&player1.contains(5)&&player1.contains(8)) winner = 1
+        if(player2.contains(2)&&player2.contains(5)&&player2.contains(8)) winner = 2
+
+        //col 3
+        if(player1.contains(3)&&player1.contains(6)&&player1.contains(9)) winner = 1
+        if(player2.contains(3)&&player2.contains(6)&&player2.contains(9)) winner = 2
+
+
+        when(winner){
+            1 -> {
+                Toast.makeText(this, "Player $winner has won", Toast.LENGTH_LONG).show()
+            }
+
+            2 -> {
+                Toast.makeText(this, "Player $winner has won", Toast.LENGTH_LONG).show()
+            }
+
         }
     }
 }
